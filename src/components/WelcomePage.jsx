8b03531befe474b2cd4b59786657c0c67178b505
LokeshAshapu <lokeshashapu@gmail.com>
2025-06-19 16:13:19 +0530

    this is second commit

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,13 +1,13 @@
+import React from 'react';
+import MainPage from './MainPage';
+
 function WelcomePage() {
     return (
     <>
         <div className="flex flex-col items-center justify-center h-screen bg-indigo-100">
-            <h1 class="text-4xl font-bold">Medicine on completion Updator</h1>
+            <h1 class="text-4xl font-bold">Welcome to Meditrack</h1>
             <p className="mt-4 text-lg text-gray-600">
                 This tool helps you update the medicine on completion.
-            </p>
-            <p className="mt-2 text-sm text-gray-500">
-                Please follow the instructions carefully.
             </p>
             <div className="mt-8">
                 <a href="" className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition duration-200 hover:shadow-lg">
