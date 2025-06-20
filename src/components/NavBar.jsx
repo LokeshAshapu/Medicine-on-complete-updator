683a1ad0e0f6f2fc526b87e060073bccd167e4d0
LokeshAshapu <lokeshashapu@gmail.com>
2025-06-20 06:57:00 +0530

    A small change in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ function NavBar() {
         <div className="container mx-auto flex justify-between items-center">
         <Link to="/main" className="text-white text-lg font-bold">MediTrack</Link>
             <div>
+                <Link to="/main" className="text-gray-300 hover:text-white px-3 py-2">Home</Link>
                 <Link to="/about" className="text-gray-300 hover:text-white px-3 py-2">About</Link>
                 <Link to="/contact" className="text-gray-300 hover:text-white px-3 py-2">Contact</Link>
             </div>
